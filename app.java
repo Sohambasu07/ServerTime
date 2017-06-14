35c5a4cae7c4598811114df396614235f2aa9db5
Sohambasu07 <soham.basu07@gmail.com>
2017-06-14 20:50:57 +0530

    Create app.java

---
+++ app.java
@@ -0,0 +1,11 @@
+import java.io.*;
+import java.lang.*;
+public class HelloWorld
+{
+public static void main()
+{
+System.out.println("Hello World");
+}
+}
+Contact GitHub API Training Shop Blog About
+© 2017 GitHub, Inc. Terms Privacy Security Status
